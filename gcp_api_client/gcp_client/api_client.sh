fbd86288d078ce31d761d6c409d6cdea422ab68c
patrik-koska <patrik.koska@protonmail.com>
2021-05-03 15:02:25 +0200

    Changed/deleted directory structure

--- gcp_api_client/gcp_client/api_client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo go run main.go --json /home/patrik_corporate/go_gcp_projects/version_controlled/gcp_client_go/gcp_api_client/gcp_client/files/basic-gcp-server.json --api-key /root/api_key
